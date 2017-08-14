e988228a0faba89f7f1434d66ea5ae0baab3ebbc
thenav56 <ayernavin@gmail.com>
2017-08-14 17:32:44 +0545

    Using http instead of socket

--- run_production.sh
+++ run_production.sh
@@ -3,5 +3,5 @@
 . /venv/bin/activate
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
-uwsgi --socket /socket/natoolbox.sock --module natoolbox.wsgi --chmod-socket=666 -H /venv
+uwsgi --http 0.0.0.0:80 --module natoolbox.wsgi --chmod-socket=666 -H /venv
 
