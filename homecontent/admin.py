bc6a2f8453bbd676a89a8a0e0d00e7c542708181
Bibek Dahal <bibekdahal.bd16@gmail.com>
2017-09-11 13:27:09 +0545

    Change the download section for single download
    Side task: complete field support request success page.

--- homecontent/admin.py
+++ homecontent/admin.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
 
 from homecontent.models import Introduction, \
-    DownloadSection, Download, KeyLink, Highlight, \
+    DownloadSection, KeyLink, Highlight, \
     Subscriber
 
 
-class DownloadInline(admin.StackedInline):
-    model = Download
+# class DownloadInline(admin.StackedInline):
+#     model = Download
 
 
-class DownloadSectionAdmin(admin.ModelAdmin):
-    inlines = [DownloadInline]
+# class DownloadSectionAdmin(admin.ModelAdmin):
+#     inlines = [DownloadInline]
 
 
 admin.site.register(Introduction)
-admin.site.register(DownloadSection, DownloadSectionAdmin)
+# admin.site.register(DownloadSection, DownloadSectionAdmin)
+admin.site.register(DownloadSection)
 admin.site.register(KeyLink)
 admin.site.register(Highlight)
 admin.site.register(Subscriber)
--- homecontent/models.py
+++ homecontent/models.py
@@ -18,6 +18,11 @@ class DownloadSection(models.Model):
     enabled = models.BooleanField(default=True)
     order = models.IntegerField(default=1)
 
+    file = models.FileField(upload_to='downloads',
+                            null=True, blank=True, default=None)
+    thumbnail = models.FileField(upload_to='download_thumbnails',
+                                 null=True, blank=True, default=None)
+
     def __str__(self):
         return self.title
 
@@ -25,22 +30,22 @@ class DownloadSection(models.Model):
         ordering = ['order']
 
 
-class Download(models.Model):
-    section = models.ForeignKey(DownloadSection)
-    title = models.CharField(max_length=200)
-    description = HTMLField(blank=True)
-    file = models.FileField(upload_to='downloads',
-                            null=True, blank=True, default=None)
-    thumbnail = models.FileField(upload_to='download_thumbnails',
-                                 null=True, blank=True, default=None)
-    enabled = models.BooleanField(default=True)
-    order = models.IntegerField(default=1)
+# class Download(models.Model):
+#     section = models.ForeignKey(DownloadSection)
+#     title = models.CharField(max_length=200)
+#     description = HTMLField(blank=True)
+#     file = models.FileField(upload_to='downloads',
+#                             null=True, blank=True, default=None)
+#     thumbnail = models.FileField(upload_to='download_thumbnails',
+#                                  null=True, blank=True, default=None)
+#     enabled = models.BooleanField(default=True)
+#     order = models.IntegerField(default=1)
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
 
-    class Meta:
-        ordering = ['order', 'title']
+#     class Meta:
+#         ordering = ['order', 'title']
 
 
 class KeyLink(models.Model):
